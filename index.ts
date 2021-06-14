9f91b37559c47c4520502531928bfefcf8b7e418
MrEnxo <somewhatenzo@gmail.com>
2021-06-14 17:58:50 -0500

    finish functions

--- index.ts
+++ index.ts
@@ -44,33 +44,90 @@ function nodeReturn(text: string) {
 	}
 	return tabs + text
 }
+function funcCall(node: ts.Node): string {
+	let args = ""
+	// @ts-expect-error
+	let node_args = node.getArguments()
+	if (node_args){
+		node_args.forEach((arg: ts.Node, index:number) => {
+			if (index < node_args.length - 1) {
+				args += parseNode(arg) + ","
+			} else {
+				args += parseNode(arg)
+			}			
+		});
+	}
+	return `${node.getFirstChildByKind(ts.SyntaxKind.Identifier)?.getText()}(${args})`
+}
+
 
 function parseNode(node: ts.Node): string {
 	switch (node.getKindName()) {
-		case 'VariableDeclaration':
+		case 'ExpressionStatement':
 			if (indent_number == 0) {
-				on_load.push("set {" + node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText() + "} to " + node.getLastChild()?.getFullText())
+				on_load.push(funcCall(node))
 				return skip
 			} else {
-				return nodeReturn("set {" + node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText() + "} to " + node.getLastChild()?.getFullText())
+				return nodeReturn(funcCall(node))
+			}
+		case 'CallExpression':
+			return nodeReturn(funcCall(node))
+		case 'StringLiteral':
+			return node.getText()
+		case 'NumericLiteral':
+			return node.getText()
+		case 'PlusToken':
+			return "+"
+		case 'VariableDeclaration':
+			if (indent_number == 0) {
+				on_load.push("set {" + node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText() + "} to " + parseNode(node.getLastChild() as ts.Node))
+				return skip
+			} else {
+				return nodeReturn("set {" + node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText() + "} to " + parseNode(node.getLastChild() as ts.Node))
 			}
 		case 'VariableDeclarationList':
 			return parseNode(node.getFirstChildByKind(ts.SyntaxKind.VariableDeclaration) as ts.Node)
 		case 'VariableStatement':
 			return parseNode(node.getFirstChildByKind(ts.SyntaxKind.VariableDeclarationList) as ts.Node)
+		case 'Identifier':
+			return `{${node.getText()}}`
+		case 'BinaryExpression':
+			let str = ""
+			node.forEachChild((child) => {
+				str += parseNode(child)
+			})
+			return str
 		case 'FunctionDeclaration':
 			let function_body = node.getFirstChildByKind(ts.SyntaxKind.Block)
+			// @ts-expect-error
+			let function_return_type = node.getReturnType().getText()
+			// @ts-expect-error
+			let functions_args = node.getParameters();
 			let func_body_str = ""
+			let args = ""
+			if (functions_args) {
+				functions_args.forEach((arg: ts.Node, index: number) => {
+					if (index < functions_args.length - 1) {
+						args += arg.getText() + ","
+					} else {
+						args += arg.getText()
+					}
+					
+				});
+			}
 			indent_number += 1
 			function_body?.forEachChild(function_node => {
 				
-				func_body_str += parseNode(function_node)
+				func_body_str += parseNode(function_node) + "\n"
 			})
 			indent_number -= 1
-			return nodeReturn(`function ${node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText()}():\n${func_body_str}`)
+			if (function_return_type) return nodeReturn(`function ${node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText()}(${args}) :: ${function_return_type}:\n${func_body_str}`)
+			return nodeReturn(`function ${node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText()}(${args}):\n${func_body_str}`)
+		case 'ReturnStatement':
+			return nodeReturn(`return ${parseNode(node.getLastChild() as ts.Node)}`)
 		default:
-			//console.log("new: " + node.type)
-			//console.log(node)
+			console.log("new: " + node.getKindName())
+		//	console.log(node.getChildren())
 		break
 	}
 	return ""
