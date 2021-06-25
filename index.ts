1e5e00349dadd2736451142bdac8a2bd0fd51bd7
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-06-25 23:25:53 +0400

    format

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const project = new ts.Project();
 let indent_number: number = 0;
 let on_load: Array<string> = [];
 let skript: string = "";
-let vars: {[key: string] : any} = {}
+let vars: { [key: string]: any; } = {};
 
 skript += convertFile(fs.readFileSync('./src/index.ts').toString());
 if (on_load.length >= 1) {
@@ -62,15 +62,15 @@ function funcCall (node: ts.Node): string {
 }
 
 //@ts-ignore
-function getAllChildrenName(node: ts.Node) { 
-	
+function getAllChildrenName (node: ts.Node) {
+
 	node.forEachChild((child) => {
-		console.log(child.getText())
-		console.log(child.getKindName())
-	})
+		console.log(child.getText());
+		console.log(child.getKindName());
+	});
 }
 
-function parseFunctionBody(node: ts.Node): string {
+function parseFunctionBody (node: ts.Node): string {
 	let function_body = node.getFirstChildByKind(ts.SyntaxKind.Block);
 	let func_body_str = "";
 	indent_number += 1;
@@ -79,15 +79,15 @@ function parseFunctionBody(node: ts.Node): string {
 		func_body_str += "\n" + parseNode(function_node);
 	});
 	indent_number -= 1;
-	return func_body_str
+	return func_body_str;
 }
 
-function parseFunction(node: ts.Node, afn?: string): string {
+function parseFunction (node: ts.Node, afn?: string): string {
 	// @ts-expect-error
 	let function_return_type = node.getReturnType().getText();
 	// @ts-expect-error
 	let functions_args = node.getParameters();
-	
+
 	let args = "";
 	if (functions_args) {
 		functions_args.forEach((arg: ts.Node, index: number) => {
@@ -106,9 +106,9 @@ function parseFunction(node: ts.Node, afn?: string): string {
 		return `function ${node.getChildrenOfKind(ts.SyntaxKind.Identifier)[0].getText()}(${args}):${func_body_str}`;
 	} else {
 		if (function_return_type != "void") return `function arrow_function_${afn}(${args}) :: ${function_return_type}:${func_body_str}`;
-		return `function arrow_function_${afn}(${args}):${func_body_str}`;		
+		return `function arrow_function_${afn}(${args}):${func_body_str}`;
 	}
-	return ""
+	return "";
 }
 
 function parseNode (node: ts.Node, format: boolean = true): string {
@@ -118,14 +118,14 @@ function parseNode (node: ts.Node, format: boolean = true): string {
 			return parseNode(node.getExpression());
 		case 'CallExpression':
 			if (node.getFirstChildByKind(ts.SyntaxKind.PropertyAccessExpression)) {
-				let expr = node.getFirstChildByKind(ts.SyntaxKind.PropertyAccessExpression)
+				let expr = node.getFirstChildByKind(ts.SyntaxKind.PropertyAccessExpression);
 				if (expr?.getFirstChild()?.getText() === vars.defaultLib) {
 					if (expr?.getText() === vars.defaultLib + ".on") {
-						let func = node.getFirstChildByKind(ts.SyntaxKind.ArrowFunction) as ts.Node
-						let body = parseFunctionBody(func)
-						let text = node.getChildAtIndex(2).getFirstChildByKind(ts.SyntaxKind.StringLiteral)?.getLiteralText()
-						return `on ${text}:${body}`
-						
+						let func = node.getFirstChildByKind(ts.SyntaxKind.ArrowFunction) as ts.Node;
+						let body = parseFunctionBody(func);
+						let text = node.getChildAtIndex(2).getFirstChildByKind(ts.SyntaxKind.StringLiteral)?.getLiteralText();
+						return `on ${text}:${body}`;
+
 					}
 				}
 
@@ -156,15 +156,15 @@ function parseNode (node: ts.Node, format: boolean = true): string {
 			});
 			return nodeReturn(str);
 		case 'FunctionDeclaration':
-			return parseFunction(node)
+			return parseFunction(node);
 		case 'ReturnStatement':
 			return nodeReturn(`return ${parseNode(node.getLastChild() as ts.Node)}`);
 		case 'EndOfFileToken':
 			return "";
 		case 'ImportDeclaration':
 			//The imported lib
-			if( node.getChildren()[3].getText().substring(1,node.getChildren()[3].getText().length - 1) === "minecraft") {
-				vars.defaultLib = node.getChildren()[1].getText()
+			if (node.getChildren()[3].getText().substring(1, node.getChildren()[3].getText().length - 1) === "minecraft") {
+				vars.defaultLib = node.getChildren()[1].getText();
 			}
 			return "";
 		default:
