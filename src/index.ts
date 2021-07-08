27f5c5a408c8fa030063652b582d9ce20eb2c873
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-07-08 14:53:10 +0400

    Write to the correct directory

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 if (!fs.existsSync("./skript/src/index.ts")) {
-	console.error('The file "./skript/src/index.ts" could not be found!');
+	console.error('The file "../skript/src/index.ts" could not be found!');
 	process.exit(1);
 }
 import { Generator } from "./generator";
 
 let gen = new Generator(fs.readFileSync("./skript/src/index.ts").toString());
 gen.parse((skript: string) => {
-	fs.writeFile("./skript/out/index.sk", skript, (err) => {
+	fs.writeFile("./skript/src/index.sk", skript, (err) => {
 		if (err) console.log(err);
 	});
 });
